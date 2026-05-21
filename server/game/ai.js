491c472e45fd9da70de57b3ec650e710f2c1d5cd
alan-g04 <nutkaalan19@gmail.com>
2026-05-21 11:34:55 +0100

    S5: server-side game logic (ported from client)

---
+++ server/game/ai.js
@@ -0,0 +1,37 @@
+const { PLAYER1, PLAYER2 } = require('./hex');
+
+const pick = arr => arr[Math.floor(Math.random() * arr.length)];
+
+class AIPlayer {
+  constructor(logic) { this.logic = logic; }
+
+  findMove(allPlayerMoves, mustJump) {
+    const keys = Object.keys(allPlayerMoves);
+    if (!keys.length) return null;
+
+    if (mustJump) {
+      const jumpable = keys.filter(k => allPlayerMoves[k].jumps.length);
+      if (!jumpable.length) return null;
+      const k = pick(jumpable);
+      const [jq, jr] = pick(allPlayerMoves[k].jumps);
+      const [q, r] = k.split(',').map(Number);
+      return [q, r, jq, jr];
+    }
+
+    const movable = keys.filter(k => allPlayerMoves[k].moves.length);
+    if (!movable.length) return null;
+    const k = pick(movable);
+    const [mq, mr] = pick(allPlayerMoves[k].moves);
+    const [q, r] = k.split(',').map(Number);
+    return [q, r, mq, mr];
+  }
+
+  findNextMultiJump(piece) {
+    const { jumps } = this.logic.getValidMoves(piece);
+    if (!jumps.length) return null;
+    const [jq, jr] = pick(jumps);
+    return [piece.q, piece.r, jq, jr];
+  }
+}
+
+if (typeof module !== 'undefined') module.exports = { AIPlayer };
